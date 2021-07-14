344313e8aa7b3a7464ba0cfebfd6be40ecf967fc
Michelle <michey.asmah21@gmail.com>
2021-07-14 17:19:01 -0400

    apollo install and user type

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,18 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql``;
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
