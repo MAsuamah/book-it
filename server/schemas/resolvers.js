699f6df7e7084fb6dcc3a43adb466ad1653c385a
Michelle <michey.asmah21@gmail.com>
2021-07-15 17:10:50 -0400

    login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { User, Book } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
+const { AuthenticationError } = require('apollo-server-express'); 
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
@@ -23,6 +23,22 @@ const resolvers = {
     
       return { token, user };
     },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+    
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const correctPw = await user.isCorrectPassword(password);
+    
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const token = signToken(user);
+      return { token, user };
+    }
   }
 };
 
