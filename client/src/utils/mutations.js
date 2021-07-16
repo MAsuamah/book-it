6474b7b8a880175fd417de42dc11e87d452f570b
Michelle <michey.asmah21@gmail.com>
2021-07-16 09:58:03 -0400

    fixed mutation.js typos

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,7 +26,7 @@ export const ADD_USER = gql`
 
 export const SAVE_BOOK = gql`
   mutation saveBook($input: savedBookInput!) {
-    saveBook($input: savedBookInput!) {
+    saveBook(input: $input) {
       _id
       username
       email
@@ -44,7 +44,7 @@ export const SAVE_BOOK = gql`
 
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: String!) {
-    removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
       _id
       username
       email
