356696e7f0b36c9d850175af8417321fab309117
Michelle <michey.asmah21@gmail.com>
2021-07-16 09:23:42 -0400

    SavedBooks rm unused variable

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -10,13 +10,12 @@ import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  /* const [userData, setUserData] = useState({}); */
 
-  const { data: userData } = useQuery(GET_ME);
-
+  const { data, loading } = useQuery(GET_ME);
+  const userData = data?.me || [];
   const [removeBook] = useMutation(REMOVE_BOOK);
-
-
+  
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -34,9 +33,9 @@ const SavedBooks = () => {
   /*     if (!response.ok) {
         throw new Error('something went wrong!');
       } */
-
+/* 
       const updatedUser = await response.json();
-      setUserData(updatedUser);
+      setUserData(updatedUser); */
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
@@ -45,7 +44,7 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
